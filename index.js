c3f81872418a70551f4faa92f86b6b8c868e00c2
Eric <egant43123@gmail.com>
2021-06-17 11:14:23 -0400

    all tests passing

--- index.js
+++ index.js
@@ -147,7 +147,7 @@ class Instructor extends Lambdasian {
     return `Today we are learning about ${subject}`
   }
   grade(student, subject){
-    return `{student.name} receives a perfect score on ${subject}`
+    return `${student.name} receives a perfect score on ${subject}`
   }
 
 }
@@ -166,9 +166,24 @@ class Instructor extends Lambdasian {
           + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
           + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   */
- class Student {
+class Student extends Lambdasian {
+  constructor(attrs){
+    super(attrs)
+    this.previousBackground = attrs.previousBackground;
+    this.className = attrs.className;
+    this.favSubjects = attrs.favSubjects;
+  }
+  listSubjects(){
+    return this.favSubjects.toString()
+  }
+  PRAssignment(subject){
+    return `${this.name} has submitted a PR for ${subject}`
+  }
+  sprintChallenge(subject){
+    return `${this.name} has begun sprint challenge on ${subject}`
+  }
      
- }
+}
   
   /*
     TASK 6
@@ -183,8 +198,18 @@ class Instructor extends Lambdasian {
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
- class ProjectManager {
-     
+class ProjectManager extends Instructor{
+  constructor(attrs){
+    super(attrs)
+    this.gradClassName = attrs.gradClassName;
+    this.favInstructor = attrs.favInstructor;
+  }
+  standUp(chan){
+    return `${this.name} announces to ${chan}, @${chan} standy times!`
+  }
+  debugsCode(student, subject){
+    return `${this.name} debugs ${student.name}'s code on ${subject}`
+  }
  }
   /*
     STRETCH PROBLEM (no tests!)
